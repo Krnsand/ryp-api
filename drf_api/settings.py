bb58c8e910b00f9239570688aeb7ee4b736bb722
Krnsand <>
2023-05-21 15:49:28 +0000

    Fix auth error

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -76,7 +76,7 @@ REST_FRAMEWORK = {
         if 'DEV' in os.environ
         else 'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
     )],
-    'DEFAULT_PAGINATION_CLASS':  'rest_framework.pagination.PageNumberPagination',
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
     'DATETIME_FORMAT': '%d %b %Y',
 }
