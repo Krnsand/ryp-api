24d8c1b7f3c1819a12548395194cf01d540f5ae5
Krnsand <krnsandin@gmail.com>
2023-06-11 12:26:35 +0000

    Change database

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -155,17 +155,17 @@ WSGI_APPLICATION = 'drf_api.wsgi.application'
 #    }
 # }
 
-if 'DEV' in os.environ:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
-else:
-    DATABASES = {
-        'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
-    }
+# if 'DEV' in os.environ:
+#    DATABASES = {
+#        'default': {
+#            'ENGINE': 'django.db.backends.sqlite3',
+#            'NAME': BASE_DIR / 'db.sqlite3',
+#        }
+#    }
+# else:
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+}
 
 
 # Password validation
